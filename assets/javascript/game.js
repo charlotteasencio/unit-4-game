25997f977c768ed4313d10e1a48bf90bfcd414f2
Charlotte Asencio <charlotte.e.asencio@gmail.com>
2018-04-04 13:22:31 -0400

    images added

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+//Define the variables
+
+var computerNumber
+var userNumber 
+var crystalA 
+var CrystalB
+var CrystalC
+var CrystalD
+var wins = 0
+var losses = 0
+
+//Computer generates a random number between 19 and 120
+
+//Crystals A-D are assigned a random number between 1 and 12. This is hidden
+
+//When each crystal is clicked, its number is added to the user #
+
+//if user# equals computer#, wins go up by one and rest of game resets
+
+//if user# is greater than computer#, losses go up and rest of game resets
+
